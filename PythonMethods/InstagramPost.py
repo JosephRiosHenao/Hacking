faa608b3afeb42e0173326d64116bd386246d95f
Joseph Rios Henao <joseph232005@gmail.com>
2020-12-02 22:00:03 -0500

    Agregando algo

--- PythonMethods/InstagramPost.py
+++ PythonMethods/InstagramPost.py
@@ -5,3 +5,4 @@ bot = Bot();
 #bot.upload_photo(photo="NewCanvas1.jpg", caption="Prueba de upload desde bot")
 bot.download_photo(media_id="https://www.instagram.com/p/CFf9DgfH_M1/?utm_source=ig_web_copy_link",filename="Download",folder="IMG")
 bot.download_photo(media_id="instagram://media?id=2404909247916667701",filename="Download2",folder="IMG")
+GGxde
